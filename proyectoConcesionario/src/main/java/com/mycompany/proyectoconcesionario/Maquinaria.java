f99d93a28466a57382145b86f0e05b2a8839eed8
Hernan Hernandez <hernanyohh@hotmail.com>
2019-02-24 11:41:46 -0500

    Clase mauinaria creada

---
+++ proyectoConcesionario/src/main/java/com/mycompany/proyectoconcesionario/Maquinaria.java
@@ -0,0 +1,85 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.proyectoconcesionario;
+
+import java.util.Scanner;
+
+/**
+ * Clase hija maquinaria que hereda de la clase Carro 
+ * @version 1.0
+ * @author Hernan
+ */
+public class Maquinaria extends Carro {
+    /**
+     *   Variable que indica la capacidad que sopota la maquinaria
+     */
+    private String capacidad;
+    
+    /**
+     *  variable que indica el tipo de llanta que debe utilizar la maquinaria 
+     */
+    private String tipoLlanta;
+    
+    Maquinaria[] nuevo;
+    
+    public Maquinaria(){
+    nuevo = new Maquinaria[20];
+    }
+
+    /**
+     * Constructor de la clase hija Maquinaria 
+     * @param capacidad
+     * @param tipoLlanta
+     * @param placa
+     * @param marca
+     * @param color
+     * @param cilindraje
+     * @param modelo
+     * @param combustible
+     * @param cantidadLlantas
+     * @param cantidadPuertas
+     * @param precio 
+     */
+    public Maquinaria(String capacidad, String tipoLlanta, int placa, String marca, String color, String cilindraje, String modelo, String combustible, int cantidadLlantas, int cantidadPuertas, double precio) {
+        super(placa, marca, color, cilindraje, modelo, combustible, cantidadLlantas, cantidadPuertas, precio);
+        this.capacidad = capacidad;
+        this.tipoLlanta = tipoLlanta;
+       
+    }
+    
+    @Override
+    public void agregarCarro(){
+        
+        Scanner S = new Scanner(System.in);
+        System.out.println("ingrese placa:");
+        setPlaca(S.nextInt());
+        System.out.println("ingrese marca:");
+        setMarca(S.next());
+        System.out.println("ingrese color:");
+        setColor(S.next());
+        System.out.println("ingresar cilindraje:");
+        setCilindraje(S.next());
+        System.out.println("ingresar modelo:");
+        setModelo(S.next());
+        System.out.println("ingresar combustible:");
+        setCombustible(S.next());
+        System.out.println("ingresar cantidad de llantas:");
+        setCantidadLlantas(S.nextInt());
+        System.out.println("ingresar cantidad de puertas:");
+        setCantidadPuertas(S.nextInt());
+        System.out.println("ingresar precio:");
+        setPrecio(S.nextDouble());
+        System.out.println("ingresar la capacidad que soporta:");
+        capacidad = S.next();
+        System.out.println("ingresar el tipo de llanta :");
+        tipoLlanta = S.next();
+        
+        nuevo[0] = new Maquinaria(capacidad,tipoLlanta,getPlaca(),getMarca(),getColor(),getCilindraje(),
+        getModelo(),getCombustible(),getCantidadLlantas(),getCantidadPuertas(),getPrecio());
+       
+        
+    }
+    }
